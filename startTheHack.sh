8ff8da1bf8e4d2505a06eb00f27d0f473d97fc13
Domenico Matteo <matteo.domenico@gmail.com>
2016-10-13 17:26:40 -0400

    start the hack script

---
+++ startTheHack.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$1" ]; then
+  echo -e "Error: No hack name supplied"
+  echo -e "\nUsage:\n./startTheHack.sh projectName"
+  exit 1
+fi
+
+git clone git@github.com:dmatteo/webpack-express-boilerplate.git $1 
+cd $1
+rm -rf .git
+
+git init
+git add .
+git commit -m 'Initial Commit'
